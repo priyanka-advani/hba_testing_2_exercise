39b4ce15003401c4e27d5bffa5c02da569f5638b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-23 13:03:26 -0700

    all flask tests passed!

---
+++ model.py
@@ -0,0 +1,38 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Board game."""
+
+    __tablename__ = "games"
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(100))
+
+
+def connect_to_db(app, db_uri="postgresql:///games"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
+
+
+def example_data():
+    """Create example data for the test database."""
+    #FIXME: write a function that creates a game and adds it to the database.
+    
+    scrabble = Game(name="Scrabble", description="Make words with letter tiles.")
+    poker = Game(name="Poker", description="Card game.")
+    majiang = Game(name="Majiang", description="Chinese tiles game.")
+
+    db.session.add_all([scrabble, poker, majiang])
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    from server import app
+
+    connect_to_db(app)
+    print "Connected to DB."
+ 
